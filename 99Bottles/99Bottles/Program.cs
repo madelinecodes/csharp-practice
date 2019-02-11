58bab720a4c00d36e8225853613fadb335df09d8
Madeline Reeves <missy11217@gmail.com>
2019-02-11 19:40:06 +0000

    Add two projects

---
+++ 99Bottles/99Bottles/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var x = 99; x > 0; x--)
+            {
+                if (x < 100 && x > 1)
+                {
+                    Console.WriteLine("{0} bottles of beer on the wall, {0} bottles of beer. Take one down, pass it around, {1} bottles of beer on the wall.", x, (x - 1));
+                }
+                if (x == 1)
+                {
+                    Console.Write(@"{0} bottle of beer on the wall {0} bottle of beer.Take one down, pass it around, no more bottles of beer on the wall. 
+                     No more bottles of beer on the wall, no more bottles of beer. 
+                     Go to the store and buy some more, 99 bottles of beer on the wall.", x);
+                }
+            }
+        }
+    }
+}
---
+++ Magic8Ball/Magic8Ball/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace Magic8Ball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ask a question or enter q to quit: ");
+            var response = Console.ReadLine();
+            if (response.ToLower() == "q")
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("Thinking...");
+                System.Threading.Thread.Sleep(1000);
+                Random random = new Random();
+                int ballRoll = random.Next(8);
+                switch (ballRoll)
+                {
+                    case 0:
+                        Console.WriteLine("It is certain");
+                        break;
+                    case 1:
+                        Console.WriteLine("Outlook good");
+                        break;
+                    case 2:
+                        Console.WriteLine("You may rely on it");
+                        break;
+                    case 3:
+                        Console.WriteLine("Ask again later");
+                        break;
+                    case 4:
+                        Console.WriteLine("Concentrate and ask again");
+                        break;
+                    case 5:
+                        Console.WriteLine("Reply hazy, try again");
+                        break;
+                    case 6:
+                        Console.WriteLine("My reply is no");
+                        break;
+                    case 7:
+                        Console.WriteLine("My sources say no");
+                        break;
+                    case 8:
+                        Console.WriteLine("Sorry, try again");
+                        break;
+                }
+            }
+        }
+    }
+}
